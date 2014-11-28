8e841d0f6ab94115390303014572bdb58a88d97c
Likit Preeyanon <preeyano@msu.edu>
2014-11-28 14:00:02 +0700

    removed bs4 module for it is not needed.

--- source/apd-to-csv.py
+++ source/apd-to-csv.py
@@ -4,15 +4,16 @@ import sys
 import re
 import csv
 import glob
-from bs4 import BeautifulSoup
 
 def main():
     for infile in glob.glob('*.txt'):
-        data = BeautifulSoup(open(infile), 'lxml')
-        html = str(data)
+        html = open(infile).read()
+
+        # use regular expression to parse all values
         aa_compositions = re.findall('[A-Z]:\s(\d+)\s,', html)
         other_values = re.findall('=\s+([-+]?[\s+]?[0-9]*\.?[0-9]+)', html)
 
+        # write values in CSV format
         writer = csv.writer(sys.stdout, 'excel-tab')
         other_values[-6] = int(other_values[-6])  # total charge
         writer.writerows([aa_compositions + other_values])

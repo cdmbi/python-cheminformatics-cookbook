87718ed4e2b48886966c0619584d64eb287f986f
Likit Preeyanon <preeyano@msu.edu>
2014-11-29 15:27:52 +0700

    removed a molecular weight from the output CSV.

--- source/apd-to-csv.py
+++ source/apd-to-csv.py
@@ -17,11 +17,13 @@ def main():
         # use regular expression to parse all values
         aa_compositions = re.findall('[A-Z]:\s(\d+)\s,', html)
         other_values = re.findall('=\s+([-+]?[\s+]?[0-9]*\.?[0-9]+)', html)
+        bohman = re.findall('is:\s([-+]?[\s+]?[0-9]*\.?[0-9]+)', html)
 
         # write values in CSV format
         writer = csv.writer(sys.stdout, 'excel-tab')
         other_values[-6] = int(other_values[-6])  # total charge
-        writer.writerows([aa_compositions + other_values])
+        writer.writerows([aa_compositions + other_values[:-3] +
+                                        other_values[-2:] + bohman])
 
 if __name__=='__main__':
     main()
